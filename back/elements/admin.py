323e1ae1b0efd3045c19c74d27694cec45d8217b
Osman <aydoganosman13@gmail.com>
2023-12-16 21:15:09 +0300

    first commit

---
+++ back/elements/admin.py
@@ -0,0 +1,39 @@
+from django.contrib import admin
+from .models import *
+
+@admin.register(Admin)
+class AdminAdmin(admin.ModelAdmin):
+    list_display = ('adminid', 'adminad', 'admineposta', 'adminsifre')
+
+@admin.register(Favoriler)
+class FavorilerAdmin(admin.ModelAdmin):
+    list_display = ('favoriid', 'kullaniciid_fav', 'kitapid_fav')
+
+@admin.register(Inceleme)
+class IncelemeAdmin(admin.ModelAdmin):
+    list_display = ('incelemeid', 'kullaniciid_inceleme', 'kitapid_inceleme', 'yazarid_inceleme', 'puan')
+
+@admin.register(Kategori)
+class KategoriAdmin(admin.ModelAdmin):
+    list_display = ('kategoriid', 'kategori')
+
+@admin.register(Kitap)
+class KitapAdmin(admin.ModelAdmin):
+    list_display = ('kitapid','kitapad','yazarid_kitap','yayintarihi')
+
+@admin.register(Kullanici)
+class KullaniciAdmin(admin.ModelAdmin):
+    list_display = ('kullaniciid', 'kullaniciadi', 'kullanicieposta', 'kullanicisifre', 'okunacaklarlistesiid_kullanici', 'okunanlarlistesiid_kullanici')
+
+@admin.register(Okunacaklarlistesi)
+class OkunacaklarlistesiAdmin(admin.ModelAdmin):
+    list_display = ('okunacaklarlistesiid', 'kitapid_okunacaklar', 'kullaniciid_okunacaklar')
+
+@admin.register(Okunanlarlistesi)
+class OkunanlarlistesiAdmin(admin.ModelAdmin):
+    list_display = ('okunanlarlistesiid', 'kitapid_okunanlar', 'kullaniciid_okunanlar')
+
+@admin.register(Yazarlar)
+class YazarlarAdmin(admin.ModelAdmin):
+    list_display = ('yazarid', 'yazaradi', 'yazardogumtarihi', 'yazarbiyografi', 'yazarfoto', 'oduller')
+
